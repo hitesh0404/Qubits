4abc23b33cbf38c0eedb1e50bb7c347fa38805c9
hitesh <hiteshkukaramprajapati@gmail.com>
2024-10-23 07:28:16 +0530

    Blocks in Html Templates

--- MainProject/accounts/views.py
+++ MainProject/accounts/views.py
@@ -7,4 +7,11 @@ def register(request):
     context = {
         'page_name' : 'Register'
     }
+    print('Hello Django here ')
     return render(request,'accounts/register.html',context)
+
+def login(request):
+    context = {
+        'page_name' : 'Log In'
+    }
+    return render(request,'accounts/login.html',context)

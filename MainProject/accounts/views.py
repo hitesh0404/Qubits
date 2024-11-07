3234f782e2725edc510f310e60ef9494fee747e3
hitesh <hiteshkukaramprajapati@gmail.com>
2024-11-07 07:39:51 +0530

    Messages

--- MainProject/accounts/views.py
+++ MainProject/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 # Create your views here.
 from django.views import View
-
+from django.contrib import messages
 def register(request):
     if request.method == 'GET':
         print(' GET method')
@@ -32,31 +32,31 @@ class Register(View):
         return render(request,'accounts/register.html',context)
 
     def post(self,request):
-        print(' POST request')
-        # username = request.POST.get('Username')
-        # password = request.POST.get('password')
-        # user = User.objects.create_superuser(username=username,password=password)
-        # print(user)
         user_form = UserRegisterForm(data=request.POST)
         customer_form = CustomerRegisterForm(data=request.POST,files=request.FILES)
+        context = {
+                'page_name' : 'Register',
+                'user_form': user_form,
+                'customer_form': customer_form,
+                }
         if user_form.is_valid() and customer_form.is_valid():
             user = user_form.save(commit=False)
-            user.password = request.POST.get('password')
+            password = request.POST.get('password')
+            confirm_password = request.POST.get('confirm_password')
+            if password==confirm_password:
+                user.password = password
+            else:
+                messages.error(request,'Password and Confirm password are not same')
+                messages.info(request,'make sure password and confirm password should be same')
+                return render(request,'accounts/register.html',context)
             user.save()
             customer = customer_form.save(commit=False)  
             customer.dob = request.POST.get('dob')
             customer.user = user
             customer.save()
-        else:
-            context = {
-            'page_name' : 'Register',
-            'user_form': user_form,
-            'customer_form': customer_form,
-            }
-            print(user_form,customer_form,request.POST,request.FILES)
+        else:            
+            messages.error(request,'Fill the Form correctly')
             return render(request,'accounts/register.html',context)
-
-            
         return redirect('/')
 
 
